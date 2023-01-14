a5c9b319f9a608ea25aa1822e474d7eb9eb3d128
Kirill-Fokin <kirill19011998@mail.ru>
2023-01-14 21:42:16 +0300

    start task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function reverse (n) {
+  let word = (String(n));
+  result = '';
   
+  
+    for (let i = word.length - 1 ; i >= 0; i-- ) {
+      if (word[i] == "-") continue;
+   result  += (String(n))[i];
+ }
+  return Number(result);
+    
 }
+
+
+
